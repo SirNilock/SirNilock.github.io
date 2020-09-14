fb8c378db93ca22f871d8f41d76a057e8b180410
SirNilock <53056611+SirNilock@users.noreply.github.com>
2020-09-14 09:15:38 -0700

    Create index.ml

---
+++ index.ml
@@ -0,0 +1,42 @@
+<!DOCTYPE html>         <!-- Made by Colin Edwards -->
+<html>
+	<head>
+		<title> Gallery</title>
+		<link rel="stylesheet" type="text/css" href="../css/nav.css"> <!-- change file path -->
+		<link rel="stylesheet" type="text/css" href="../css/colin.css">
+	</head>
+	<body>
+		<div class="all">
+		<img src="../IMG/top.jpg" class="top">	<!--of nav-->
+		<div class="bar">										
+			<a href="../Main.html" class="logo"> 
+			<div>
+				<img class="logoimg" src="../IMG/pillar.png">
+				<h2 class="logotxt">Bob Ross </h2>
+				<p class="logotxttwo" id="down">Of Seattle</p>
+			</div> </a>
+			<div class="navbox">					<!--change on main -->
+				<a href="../html/contact.html"><div class="nav">Contact</div></a>
+				<a href="../html/Bio.html"><div class="nav">Bio </div></a>
+				<a href="../html/Schedule.html"><div class="nav">Schedule</div></a>
+				<a href="../html/Map.html"><div class="nav">Map</div></a>
+				<a href="../html/About.html"><div class="nav">About us</div></a>
+				<a href="../html/Ticket.html"><div class="nav">Tickets</div></a>
+				<a href="../html/Gallery.html"><div class="nav">Gallery </div></a>
+			</div>
+		</div>											
+		<div class="space"> </div>
+	</div>			<!-- end of nav-->
+
+		<div class="info" id="ex">
+			<img src="../IMG/tree.jpg" class="pic">
+			<h3>An un-named painting by Bob Ross made for his popular TV show The Joy of Painting</h3>
+		</div>
+		<div class="info" id="ex">
+			<img src="../IMG/waterfall.jpg" class="pic">
+			<h3> Waterfall: a painting by Bob Ross</h3>
+		</div>
+		
+		
+	</head>
+</html>
